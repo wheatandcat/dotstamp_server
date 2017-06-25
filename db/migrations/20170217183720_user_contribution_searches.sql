f751f51037f6d3749ac94f10ebbbf21a125b9f92
“wheatandcat” <wheatandcat@gmail.com>
2017-06-25 20:37:22 +0900

    test mysql

--- db/migrations/20170217183720_user_contribution_searches.sql
+++ db/migrations/20170217183720_user_contribution_searches.sql
@@ -1,6 +1,12 @@
 
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
+INSTALL PLUGIN Mroonga SONAME 'ha_mroonga.so';
+CREATE FUNCTION last_insert_grn_id RETURNS INTEGER SONAME 'ha_mroonga.so';
+CREATE FUNCTION mroonga_snippet RETURNS STRING SONAME 'ha_mroonga.so';
+CREATE FUNCTION mroonga_command RETURNS STRING SONAME 'ha_mroonga.so';
+CREATE FUNCTION mroonga_escape RETURNS STRING SONAME 'ha_mroonga.so';
+
 CREATE TABLE IF NOT EXISTS `user_contribution_searches` (
     `id` BIGINT NOT NULL AUTO_INCREMENT,
     `user_contribution_id` BIGINT NOT NULL COMMENT 'ユーザ投稿ID',
