452820ce282034ba7c1e156b53e0fa8eb44348c8
“wheatandcat” <wheatandcat@gmail.com>
2017-07-19 03:29:43 +0900

    url

--- models/user_contribution_sound_detail.go
+++ models/user_contribution_sound_detail.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"strconv"
+	"time"
 
 	"github.com/jinzhu/gorm"
 )
@@ -19,14 +20,17 @@ const (
 
 // UserContributionSoundDetail ユーザ投稿音声
 type UserContributionSoundDetail struct {
-	gorm.Model
-	UserContributionID int    `json:"user_contribution_id"`
-	Priority           int    `json:"priority"`
-	TalkType           int    `json:"talk_type"`
-	Body               string `json:"body"`
-	BodySound          string `json:"body_sound"`
-	VoiceType          int    `json:"voice_type"`
-	MakeStatus         int    `json:"make_status"`
+	ID                 uint       `gorm:"primary_key" json:"id"`
+	UserContributionID int        `json:"user_contribution_id"`
+	Priority           int        `json:"priority"`
+	TalkType           int        `json:"talk_type"`
+	Body               string     `json:"body"`
+	BodySound          string     `json:"body_sound"`
+	VoiceType          int        `json:"voice_type"`
+	MakeStatus         int        `json:"make_status"`
+	CreatedAt          time.Time  `json:"createdAt"`
+	UpdatedAt          time.Time  `json:"updatedAt"`
+	DeletedAt          *time.Time `json:"deletedAt"`
 }
 
 // Add 追加する
